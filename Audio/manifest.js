362b0f29110ab30dc3ec13c3c5e43cb6660fe051
Girish <girishproject28@gmail.com>
2026-06-13 15:17:48 +0530

    Add SHAC project files

---
+++ Audio/manifest.js
@@ -0,0 +1,44 @@
+window.MANIFEST_RAW = [
+  {
+    codec: "dac_16khz",
+    gt_id: "40840_AV",
+    source: "CirCor DigiScope",
+    gt_url: "Audio/real/40840_AV.wav",
+    gen_url: "Audio/fake/dac_16khz/40840_AV_dac_16khz.wav"
+  },
+  {
+    codec: "encodec_24khz",
+    gt_id: "50115_PV_1",
+    source: "CirCor DigiScope",
+    gt_url: "Audio/real/50115_PV_1.wav",
+    gen_url: "Audio/fake/encodec_24khz/50115_PV_1_facebook_encodec_24khz.wav"
+  },
+  {
+    codec: "snac_24khz",
+    gt_id: "50277_PV",
+    source: "CirCor DigiScope",
+    gt_url: "Audio/real/50277_PV.wav",
+    gen_url: "Audio/fake/snac_24khz/50277_PV_hubertsiuzdak_snac_24khz.wav"
+  },
+  {
+    codec: "speechtokenizer",
+    gt_id: "68260_AV",
+    source: "CirCor DigiScope",
+    gt_url: "Audio/real/68260_AV.wav",
+    gen_url: "Audio/fake/speechtokenizer/68260_AV_speechtokenizer.wav"
+  },
+  {
+    codec: "funcodec_16khz",
+    gt_id: "68827_TV",
+    source: "CirCor DigiScope",
+    gt_url: "Audio/real/68827_TV.wav",
+    gen_url: "Audio/fake/funcodec_16khz/68827_TV_funcodec_16khz.wav"
+  },
+  {
+    codec: "audiodec_libritts_v1",
+    gt_id: "76240_MV",
+    source: "CirCor DigiScope",
+    gt_url: "Audio/real/76240_MV.wav",
+    gen_url: "Audio/fake/audiodec_libritts_v1/76240_MV_AudioDec_libritts_v1_24khz.wav"
+  }
+];
